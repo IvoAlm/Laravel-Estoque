a10badb82c11eb4cd9077b0ef350ae7d18b61466
traineeAS <trainee@actualsales.com.br>
2018-12-04 17:29:21 -0200

    Add um form para novos produtos

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 
 Route::get('/produtos', 'ProdutoController@lista');
 Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra')->where('id','[0-9]+');
+Route::get('/produtos/novo', 'ProdutoController@novo');
